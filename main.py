a2e3d8ebda8c2d8aeb96406ce8821664058a11c5
alex <yuting09120310@hotmail.com>
2021-12-26 18:57:06 +0800

    初版

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import json
 
 
 
-with open('test.json',encoding = 'utf8') as f:
+with open('2021_12_23.json',encoding = 'utf8') as f:
     data = json.load(f)
 
-    def search():
-        return [datum for datum in data ]
+    for i in data:
+        item = list(i.values())
+        TrainNO = " '" + item[1]["TrainNo"] + "' ,"
+        StationName = " '" + item[2]["StationName"]["Zh_tw"] + "' ,"
+        ArrivalTime = " '" + item[2]["ArrivalTime"] + "' ,"
+        EndName = " '" + item[3]["StationName"]["Zh_tw"] + "' ,"
+        EndTime = " '" + item[3]["ArrivalTime"]+ "' ,"
 
-    for dict in data:
-        datum = search()
-        print(datum['No'])
+        print(TrainNO + StationName + ArrivalTime + EndName + EndTime)
